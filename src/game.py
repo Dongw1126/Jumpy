962259dd8368d7be1e32c81f923536a2fb9085d5
Dongw1126 <topblade71@naver.com>
2019-04-04 14:02:25 +0000

    add resource

--- src/game.py
+++ src/game.py
@@ -1,2 +1,20 @@
 import pygame
-print(pygame.init())
+from pygame.locals import *
+
+SIZE = WIDTH, HEIGHT = 800, 600
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, position):
+        pygame.sprite.Sprite.__init__(self)
+        self.userPosition = user_position
+        self.userImage = []
+
+    def update(self):
+        self.rect = self.image.get_rect()
+        self.rect.center = self.user_position
+
+    def load_image(self, file):
+        idleSprite = ['idle-2-0' + str(i) + '.png' for i in range(4)]
+        idleImage = [pygame.image.load(file + i for i in idle_sprite]
+        self.user_image.append(idleImage)
