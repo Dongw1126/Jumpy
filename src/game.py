cfae4e7b048723fb48575ab4c4f76484c5e4f7a4
Dongw1126 <topblade71@naver.com>
2019-04-05 13:47:47 +0000

    Implement walking motion

--- src/game.py
+++ src/game.py
@@ -10,27 +10,65 @@ class Player(pygame.sprite.Sprite):
         self.user_position = [position[0], position[1]]
         self.user_image = []
         self.loadImage(path)
+        self.vel_left = False
+        self.event_list = [False for i in range(EVENT)]
 
     def update(self, screen):
         global frame
-        pressed = pygame.key.get_pressed()
-        if pressed[pygame.K_UP]:
-            self.user_position[1] -= 3
-        elif pressed[pygame.K_DOWN]:
-            self.user_position[1] += 3
-        elif pressed[pygame.K_LEFT]:
-            self.user_position[0] -= 3
-        elif pressed[pygame.K_RIGHT]:
-            self.user_position[0] += 3
-
-        frame += 1
-        point = self.user_position
-        screen.blit(self.user_image[IDLE][frame % 4], self.user_position)
+        self.EventHandler()
+        if self.event_list[IDLERIGHT]:
+            screen.blit(self.user_image[IDLERIGHT][int(frame/4) % 4],
+                        self.user_position)
+        elif self.event_list[IDLELEFT]:
+            screen.blit(self.user_image[IDLELEFT][int(frame/4) % 4],
+                        self.user_position)
+        elif self.event_list[WALKRIGHT]:
+            screen.blit(self.user_image[WALKRIGHT][int(frame/4) % 5],
+                        self.user_position)
+        elif self.event_list[WALKLEFT]:
+            screen.blit(self.user_image[WALKLEFT][int(frame/4) % 5],
+                        self.user_position)
 
     def loadImage(self, path):
         idle_sprite = ['idle-2-0' + str(i) + '.png' for i in range(4)]
-        idle_image = [pygame.image.load(path + i) for i in idle_sprite]
-        self.user_image.append(idle_image)
+        idle_right_image = [pygame.image.load(path + i).convert_alpha()
+                            for i in idle_sprite]
+        idle_left_image = [pygame.transform.flip(i, True, False)
+                           for i in idle_right_image]
+
+        walk_right_sprite = ['run-0' + str(i) + '.png' for i in range(5)]
+        walk_right_image = [pygame.image.load(path + i).convert_alpha()
+                            for i in walk_right_sprite]
+        walk_left_image = [pygame.transform.flip(i, True, False)
+                           for i in walk_right_image]
+
+        # The append order is the same as the constant declaration!!
+        self.user_image.append(idle_right_image)
+        self.user_image.append(idle_left_image)
+        self.user_image.append(walk_right_image)
+        self.user_image.append(walk_left_image)
+
+    def EventHandler(self):
+        pressed = pygame.key.get_pressed()
+        self.event_list = [False for i in range(EVENT)]
+
+        if pressed[pygame.K_RIGHT]:
+            self.user_position[0] += 3
+            self.event_list[WALKRIGHT] = True
+            self.vel_left = False
+
+        elif pressed[pygame.K_LEFT]:
+            self.user_position[0] -= 3
+            self.event_list[WALKLEFT] = True
+            self.vel_left = True
+
+        else:
+            if self.vel_left:
+                self.event_list[IDLELEFT] = True
+                self.event_list[IDLERIGHT] = False
+            else:
+                self.event_list[IDLERIGHT] = True
+                self.event_list[IDLELEFT] = False
 
 
 class Game:
@@ -40,6 +78,7 @@ class Game:
         self.clock = pygame.time.Clock()
 
     def main(self):
+        global frame
         pygame.init()
         pygame.display.set_caption("Test")
 
@@ -47,8 +86,8 @@ class Game:
         player = Player("res/Char/", rect.center)
 
         while True:
-            time = self.clock.tick(15)
-
+            time = self.clock.tick(60)
+            frame += 1
             self.screen.fill((0, 0, 0))
 
             player.update(self.screen)
