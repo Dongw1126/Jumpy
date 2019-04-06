5195649b3af901c7b93eabc6d149a6b60abbfdd1
Dongw1126 <topblade71@naver.com>
2019-04-06 07:02:06 +0000

    fix : Rewrite all codes feat : Implement physics (acc, gravity)

---
+++ src/settings.py
@@ -0,0 +1,17 @@
+# game options/settings
+TITLE = "TEST"
+WIDTH = 800
+HEIGHT = 600
+FPS = 60
+
+# Player properties
+PLAYER_ACC = 1.5
+PLAYER_FRICTION = -0.2
+
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
