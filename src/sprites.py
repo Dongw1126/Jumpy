dd73b5a64d86a939714b5f97c295c589784fc793
Dongw1126 <topblade71@naver.com>
2019-04-07 04:38:20 +0000

    feat : Add Walking animation, Platform Image

--- src/sprites.py
+++ src/sprites.py
@@ -1,5 +1,6 @@
 import pygame as pg
 from settings import *
+from random import choice
 vec = pg.math.Vector2
 
 
@@ -30,22 +31,20 @@ class Player(pg.sprite.Sprite):
         self.image = self.standing_frames[0]
 
         self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH/2, HEIGHT/2)
+        self.rect.center = (40, HEIGHT - 100)
 
-        self.pos = vec(WIDTH/2, HEIGHT/2)
+        self.pos = vec(40, HEIGHT - 100)
         self.vel = vec(0, 0)
         self.acc = vec(0, 0)
 
     def load_images(self):
-        self.standing_frames =
-        [self.game.spritesheet.get_image(614, 1063, 120, 191),
-         self.game.spritesheet.get_image(690, 406, 120, 201)]
+        self.standing_frames = [self.game.spritesheet.get_image(614, 1063, 120, 191),
+                                self.game.spritesheet.get_image(690, 406, 120, 201)]
         for frame in self.standing_frames:
             frame.set_colorkey(BLACK)
 
-        self.walk_frames_r =
-        [self.game.spritesheet.get_image(678, 860, 120, 201),
-         self.game.spritesheet.get_image(692, 1458, 120, 207)]
+        self.walk_frames_r = [self.game.spritesheet.get_image(678, 860, 120, 201),
+                              self.game.spritesheet.get_image(692, 1458, 120, 207)]
         for frame in self.walk_frames_r:
             frame.set_colorkey(BLACK)
 
@@ -75,17 +74,39 @@ class Player(pg.sprite.Sprite):
 
         self.acc.x += self.vel.x*PLAYER_FRICTION
         self.vel += self.acc
+        if abs(self.vel.x) < 0.1:
+            self.vel.x = 0
         self.pos += self.vel + 0.5*self.acc
 
-        if self.pos.x > WIDTH:
-            self.pos.x = WIDTH
-        if self.pos.x < 0:
-            self.pos.x = 0
+        if self.pos.x > WIDTH + self.rect.width/2:
+            self.pos.x = 0 - self.rect.width/2
+        if self.pos.x < 0 - self.rect.width/2:
+            self.pos.x = WIDTH + self.rect.width/2
 
         self.rect.midbottom = self.pos
 
     def animate(self):
         now = pg.time.get_ticks()
+
+        if self.vel.x != 0:
+            self.walking = True
+        else:
+            self.walking = False
+
+        if self.walking:
+            if now - self.last_update > 200:
+                self.last_update = now
+                self.current_frame = ((self.current_frame + 1)
+                                      % len(self.walk_frames_l))
+
+                bottom = self.rect.bottom
+                if self.vel.x > 0:
+                    self.image = self.walk_frames_r[self.current_frame]
+                else:
+                    self.image = self.walk_frames_l[self.current_frame]
+                    self.rect = self.image.get_rect()
+                    self.rect.bottom = bottom
+
         if not self.jumping and not self.walking:
             if (now - self.last_update) > 350:
                 self.last_update = now
@@ -98,10 +119,15 @@ class Player(pg.sprite.Sprite):
 
 
 class Platform(pg.sprite.Sprite):
-    def __init__(self, x, y, w, h):
+    def __init__(self, game, x, y):
         pg.sprite.Sprite.__init__(self)
-        self.image = pg.Surface((w, h))
-        self.image.fill(GREEN)
+        self.game = game
+
+        images = [self.game.spritesheet.get_image(0, 288, 380, 94),
+                  self.game.spritesheet.get_image(213, 1662, 201, 100)]
+
+        self.image = choice(images)
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
